4bda67aa754c3b4054d122552d8eedae3c76a8f3
Ivan_Andrianov <andreyanovi@yandex.ru>
2023-05-01 14:31:42 +0300

    update

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,8 @@ import json
 
 class Raf9:
     def __init__(self):
-        self.ingridients = ['lemon', 'soda', 'orange', 'ice']
+        self.ingridients = ['lemon', 'soda', 'orange', 'ice', 'mint']
+        self.get_coctails_from_db()
 
     def __call__(self, *args, **kwargs):
         while True:
@@ -14,23 +15,37 @@ class Raf9:
                 break
             elif command == '1':
                 current_ings = self.choose_ingridients()
-                self.find_coctail(current_ings)
+                chose_coctail = self.find_coctail(current_ings)
+                if chose_coctail is None:
+                    self.save_coctail(current_ings)
+                else:
+                    print(f'You choose {chose_coctail} coctail')
             else:
                 print("Не знаю такой команды")
-
 
     def __help_text(self):
         print("Доступные команды:")
         print("1- выбрать ингредиент")
         print("0- выход")
 
-    def get_coctails_from_db(self)
+    def save_coctail(self, current_ings):
+        self.coctails.append({
+            'name': 'unnamed',
+            'ingridients': current_ings
+        })
+
+        with open('coctails.json', 'w') as json_file:
+            json.dump(self.coctails, json_file)
+
+    def get_coctails_from_db(self):
         with open('coctails.json', 'r') as json_file:
             self.coctails = json.load(json_file)
-            pass
 
     def find_coctail(self, current_ings):
-        pass
+        for c in self.coctails:
+            if c.get('ingridients') == current_ings:
+                return c.get('name')
+        return None
 
     def choose_ingridients(self):
         choosed_ingr = []
@@ -49,20 +64,11 @@ class Raf9:
                     if number > len(self.ingridients):
                         print('Такого ингредиента в списке нет')
                     else:
-                        choosed_ingr.append(self.ingridients[number-1])
+                        choosed_ingr.append(self.ingridients[number - 1])
                 else:
                     print("Введите номер ингредиента")
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
     raf9 = Raf9()
     raf9()
-
